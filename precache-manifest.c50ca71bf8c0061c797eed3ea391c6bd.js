8003be09cb0f5e7607f267f761d402dfd49724e6
unknown <matej.pavic92@gmail.com>
2020-09-03 09:21:58 +0200

    Updates

---
+++ precache-manifest.c50ca71bf8c0061c797eed3ea391c6bd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fb9d779f237b434bdd9059feb7f7f7",
+    "url": "/propisi/index.html"
+  },
+  {
+    "revision": "c274f1a27414f37bc6ba",
+    "url": "/propisi/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "83a8b781e810d598a933",
+    "url": "/propisi/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c274f1a27414f37bc6ba",
+    "url": "/propisi/static/js/2.3dd01131.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/propisi/static/js/2.3dd01131.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a8b781e810d598a933",
+    "url": "/propisi/static/js/main.942c5ece.chunk.js"
+  },
+  {
+    "revision": "9386c5577fc48210433d",
+    "url": "/propisi/static/js/runtime-main.00ef47b5.js"
+  },
+  {
+    "revision": "cab7700ae324e09193a9cbc4eaf4cc53",
+    "url": "/propisi/static/media/autoskolaL.cab7700a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.571ebba57007b35d096cd41be493e61a.js"
+  "/propisi/precache-manifest.c50ca71bf8c0061c797eed3ea391c6bd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/propisi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
